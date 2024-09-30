8c698db42d9697c215795ca0cbaa9d4ed56578c0
zen0 <79776344+zen0dev@users.noreply.github.com>
2024-09-29 22:49:19 -0600

    realm requests from forge working

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 export type { Router as ShardRouter } from './shard/shard.router';
-export { createRouter as createShardClientRouter } from './client/shard.client';
-export type { Router as ShardClientRouter } from './client/shard.client';
+export { createRouter as createShardClientRouter } from './client/client.router';
+export type { Router as ShardClientRouter } from './client/client.router';
 export * as Realm from './realm/realm.types';
 export * as Shard from './shard/shard.types';
